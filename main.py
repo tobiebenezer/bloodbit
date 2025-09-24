d1d70d17ab9c12d0e38da1c4dde88883345de7a0
Tobi Ebenezer <tobisamuel70@gmail.com>
2025-09-24 12:28:31 +0200

    cors

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from flask_cors import CORS
 
 def create_app(config_overrides=None):
     app = Flask(__name__)
-    CORS(app)
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+    
     app.config.from_object('config.Config')
 
     if config_overrides:
